dd2c1693c80479c936f43fa69db0d6b2b1c9abab
Phạm Lê Khánh Duy <phamduy2408@gmail.com>
2023-10-31 14:13:47 +0700

    add public id and folder

--- src/providers/upload.provider.ts
+++ src/providers/upload.provider.ts
@@ -2,7 +2,10 @@ import cloudinary from '~/config/cloudinary.config'
 import { MulterFile } from '~/types/MulterFile'
 
 const uploadImage = async (file: MulterFile) => {
-    const result = await cloudinary.uploader.upload(file.path)
+    const result = await cloudinary.uploader.upload(file.path, {
+        folder: 'avatars',
+        public_id: file.filename.slice(0, file.filename.lastIndexOf('.'))
+    })
     return result
 }
 
